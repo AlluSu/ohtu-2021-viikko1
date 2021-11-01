c7018b9d7dfed55bd31057ed4049c95cad03663f
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2021-11-01 17:01:04 +0200

    Lisätty testejä 100% kattavuutta varten

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,36 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_nolla_tilavuus_nollaa(self):
+        self.varasto = Varasto(0)
+        
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+    
+    def test_negatiivinen_tilavuus_nollaa(self):
+        self.varasto = Varasto(-10)
+        
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_negatiivinen_saldo_nollaa(self):
+        self.varasto = Varasto(10, -1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_negatiivista_lukua_ei_voi_ottaa_varastosta(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-2), 0)
+
+    def test_otetaan_isompi_kuin_saldo(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(2), 0)
+    
+    def test_negatiivinen_lisays_ei_tee_mitaan(self):
+        maara = self.varasto.lisaa_varastoon(-1)
+
+        self.assertIsNone(maara)
+
+    def test_maara_enemman_kuin_mita_mahtuu_tayttaa_kokonaan(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_tiedot_tulostuu_oikein(self):
+        self.assertEqual(self.varasto.__str__(), "saldo = 0, vielä tilaa 10")
